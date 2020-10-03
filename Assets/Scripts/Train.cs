4b778f854641da66abade34ed79977cfd3de67ef
Sarel <sarelmicha93@gmail.com>
2020-10-03 14:02:52 +0300

    added train

---
+++ Assets/Scripts/Train.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    [SerializeField] private float speed =  2f;
+    [SerializeField] private float breakSpeed = 0.05f;
+
+    private bool isBreaksOn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBreaksOn)
+        {
+            Drive(breakSpeed);
+        }
+        else
+        {
+            Drive(1f);
+        }
+       
+    }
+
+    private void Drive(float breakSpeed)
+    {
+        transform.position +=  Vector3.forward  * Time.deltaTime * speed * breakSpeed;
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Station")
+        {
+            isBreaksOn = true;
+        }
+        else if (other.tag == "Statue")
+        {
+            speed = 0;
+        }
+    }
+
+}
