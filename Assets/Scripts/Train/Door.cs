f5ec390826823582e97be82e74cff7361fa45a5a
Sarel <sarelmicha93@gmail.com>
2020-10-03 19:17:44 +0300

    added npc

---
+++ Assets/Scripts/Train/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator anim = null;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Person")
+        {
+            anim.SetTrigger("open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Person")
+        {
+            anim.SetTrigger("close");
+        }
+    }
+}
---
+++ Assets/Scripts/Train/Person.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Person : MonoBehaviour
+{
+    [SerializeField] GameObject[] cells = null;
+
+    private NavMeshAgent navMesh = null;
+    private Animator anim = null;
+
+    private void Awake()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (transform.position == navMesh.destination)
+        {
+            //anim.SetTrigger("idle");
+            print("im in destiniation");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Train")
+        {
+            print("lets walk");
+            navMesh.destination = cells[Random.Range(0, cells.Length)].transform.position;
+            anim.SetTrigger("walk");
+        }
+    }
+}
